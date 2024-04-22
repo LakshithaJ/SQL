e36392033e81befbac218abb0094b3d0aad7d62f
LakshithaRJ <lakshrj139@gmail.com>
2024-04-22 15:23:30 -0500

    UNION

--- test.sql
+++ test.sql
@@ -189,3 +189,27 @@ SELECT Customers.CustomerName, Orders.OrderID
 FROM Customers
 FULL OUTER JOIN Orders ON Customers.CustomerID=Orders.CustomerID
 ORDER BY Customers.CustomerName;
+
+
+-- The SQL UNION Operator
+-- The UNION operator is used to combine the result-set of two or more SELECT statements.
+-- *Every SELECT statement within UNION must have the same number of columns
+-- *The columns must also have similar data types
+-- *The columns in every SELECT statement must also be in the same order
+
+-- UNION Syntax
+-- SELECT column_name(s) FROM table1
+-- UNION
+-- SELECT column_name(s) FROM table2;
+
+-- UNION ALL Syntax
+-- The UNION operator selects only distinct values by default. To allow duplicate values, use UNION ALL:
+-- SELECT column_name(s) FROM table1
+-- UNION ALL
+-- SELECT column_name(s) FROM table2;
+
+-- The following SQL statement returns the cities (only distinct values) from both the "Customers" and the "Suppliers" table:
+SELECT City FROM Customers
+UNION
+SELECT City FROM Suppliers
+ORDER BY City;
