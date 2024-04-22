12c41b5e8a1abf54748676b5cda0d63a8acdc93c
LakshithaRJ <lakshrj139@gmail.com>
2024-04-22 17:30:21 -0500

    Python MySQL Order By

--- mysql.py
+++ mysql.py
@@ -105,3 +105,20 @@ myresult = mycursor.fetchall()
 for x in myresult:
   print(x)
 
+# Sort the Result
+# Use the ORDER BY statement to sort the result in ascending or descending order
+# The ORDER BY keyword sorts the result ascending by default. To sort the result in descending order, use the DESC keyword.
+sql = "SELECT * FROM customers ORDER BY name"
+mycursor.execute(sql)
+myresult = mycursor.fetchall()
+
+for x in myresult:
+  print(x)
+
+# Sort the result reverse alphabetically by name:
+sql = "SELECT * FROM customers ORDER BY name DESC"
+mycursor.execute(sql)
+myresult = mycursor.fetchall()
+
+for x in myresult:
+  print(x)
