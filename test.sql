66bec7d771f870e47c6959cbea0c53bf6a93024b
LakshithaRJ <lakshrj139@gmail.com>
2024-04-22 15:08:25 -0500

    SQL JOIN

--- test.sql
+++ test.sql
@@ -125,3 +125,67 @@ SELECT * FROM Customers AS Persons;
 SELECT o.OrderID, o.OrderDate, c.CustomerName
 FROM Customers AS c, Orders AS o
 WHERE c.CustomerName='Around the Horn' AND c.CustomerID=o.CustomerID;
+
+-- SQL JOIN
+-- A JOIN clause is used to combine rows from two or more tables, based on a related column between them.
+
+-- Different Types of SQL JOINs
+-- (INNER) JOIN: Returns records that have matching values in both tables
+-- SELECT column_name(s)
+-- FROM table1
+-- INNER JOIN table2
+-- ON table1.column_name = table2.column_name;
+-- JOIN and INNER JOIN will return the same result.
+-- INNER is the default join type for JOIN, so when you write JOIN the parser actually writes INNER JOIN
+
+SELECT ProductID, ProductName, CategoryName
+FROM Products
+INNER JOIN Categories ON Products.CategoryID = Categories.CategoryID;
+
+-- JOIN Three Tables
+SELECT Orders.OrderID, Customers.CustomerName, Shippers.ShipperName
+FROM ((Orders
+INNER JOIN Customers ON Orders.CustomerID = Customers.CustomerID)
+INNER JOIN Shippers ON Orders.ShipperID = Shippers.ShipperID);
+
+-- LEFT (OUTER) JOIN: Returns all records from the left table, and the matched records from the right table
+-- Note: The LEFT JOIN keyword returns all records from the left table(1), even if there are no matches in the right table(2).
+-- LEFT JOIN Syntax
+-- SELECT column_name(s)
+-- FROM table1
+-- LEFT JOIN table2
+-- ON table1.column_name = table2.column_name;
+
+-- The following SQL statement will select all customers, and any orders they might have:
+SELECT Customers.CustomerName, Orders.OrderID
+FROM Customers
+LEFT JOIN Orders ON Customers.CustomerID = Orders.CustomerID
+ORDER BY Customers.CustomerName;
+
+-- RIGHT (OUTER) JOIN: Returns all records from the right table, and the matched records from the left table
+-- Note: The RIGHT JOIN keyword returns all records from the right table(2), even if there are no matches in the left table(1).
+-- RIGHT JOIN Syntax
+-- SELECT column_name(s)
+-- FROM table1
+-- RIGHT JOIN table2
+-- ON table1.column_name = table2.column_name;
+
+-- The following SQL statement will return all employees, and any orders they might have placed:
+SELECT Orders.OrderID, Employees.LastName, Employees.FirstName
+FROM Orders
+RIGHT JOIN Employees ON Orders.EmployeeID = Employees.EmployeeID
+ORDER BY Orders.OrderID;
+
+-- FULL (OUTER) JOIN: Returns all records when there is a match in either left or right table
+-- FULL OUTER JOIN Syntax
+-- SELECT column_name(s)
+-- FROM table1
+-- FULL OUTER JOIN table2
+-- ON table1.column_name = table2.column_name
+-- WHERE condition;
+
+-- The following SQL statement selects all customers, and all orders:
+SELECT Customers.CustomerName, Orders.OrderID
+FROM Customers
+FULL OUTER JOIN Orders ON Customers.CustomerID=Orders.CustomerID
+ORDER BY Customers.CustomerName;
