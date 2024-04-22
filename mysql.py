3e8b4d497781bb36de55d29e45b597318e85050f
LakshithaRJ <lakshrj139@gmail.com>
2024-04-22 17:37:29 -0500

    Python MySQL Limit

--- mysql.py
+++ mysql.py
@@ -162,3 +162,20 @@ mycursor.execute(sql, val)
 mydb.commit()
 
 print(mycursor.rowcount, "record(s) affected")
+
+# Select the 5 first records in the "customers" table:
+mycursor.execute("SELECT * FROM customers LIMIT 5")
+myresult = mycursor.fetchall()
+
+for x in myresult:
+  print(x)
+
+# Start From Another Position
+# If you want to return five records, starting from the third record, you can use the "OFFSET" keyword:
+# Start from position 3, and return 5 records:
+mycursor.execute("SELECT * FROM customers LIMIT 5 OFFSET 2")
+
+myresult = mycursor.fetchall()
+
+for x in myresult:
+  print(x)
