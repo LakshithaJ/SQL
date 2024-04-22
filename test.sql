e6a0363bc9918e8d14d8a84f35f369e48e7d8d4a
LakshithaRJ <lakshrj139@gmail.com>
2024-04-22 11:40:45 -0500

    SQL BETWEEN Operator

--- test.sql
+++ test.sql
@@ -42,3 +42,39 @@ WHERE CustomerName LIKE 'a__%';
 -- Return all customers that have "r" in the second position:
 SELECT * FROM Customers
 WHERE CustomerName LIKE '_r%';
+
+-- SQL IN Operator
+-- The IN operator allows you to specify multiple values in a WHERE clause.
+-- The IN operator is a shorthand for multiple OR conditions.
+
+-- Syntax
+SELECT column_name(s)
+FROM table_name
+WHERE column_name IN (value1, value2, ...);
+
+-- Return all customers from 'Germany', 'France', or 'UK'
+SELECT * FROM Customers
+WHERE Country IN ('Germany', 'France', 'UK');
+
+-- SQL BETWEEN Operator
+-- The BETWEEN operator selects values within a given range. The values can be numbers, text, or dates.
+-- The BETWEEN operator is inclusive: begin and end values are included. 
+
+-- Selects all products with a price between 10 and 20:
+SELECT * FROM Products
+WHERE Price BETWEEN 10 AND 20;
+
+SELECT * FROM Products
+WHERE Price NOT BETWEEN 10 AND 20;
+
+-- The following SQL statement selects all products with a ProductName alphabetically between Carnarvon Tigers and Mozzarella di Giovanni:
+SELECT * FROM Products
+WHERE ProductName BETWEEN 'Carnarvon Tigers' AND 'Mozzarella di Giovanni'
+ORDER BY ProductName;
+
+-- The following SQL statement selects all orders with an OrderDate between '01-July-1996' and '31-July-1996':
+SELECT * FROM Orders
+WHERE OrderDate BETWEEN #07/01/1996# AND #07/31/1996#;
+
+SELECT * FROM Orders
+WHERE OrderDate BETWEEN '1996-07-01' AND '1996-07-31';
