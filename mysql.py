d3b50497404bc58258eecdbb6da4eaebf55136b3
LakshithaRJ <lakshrj139@gmail.com>
2024-04-22 17:33:25 -0500

    Python MySQL Delete

--- mysql.py
+++ mysql.py
@@ -122,3 +122,20 @@ myresult = mycursor.fetchall()
 
 for x in myresult:
   print(x)
+
+# Delete Record
+# You can delete records from an existing table by using the "DELETE FROM" statement:
+# Delete any record where the address is "Mountain 21":
+sql = "DELETE FROM customers WHERE address = 'Mountain 21'"
+mycursor.execute(sql)
+mydb.commit()
+
+print(mycursor.rowcount, "record(s) deleted")
+
+# Escape values by using the placeholder %s method:
+sql = "DELETE FROM customers WHERE address = %s"
+adr = ("Yellow Garden 2", )
+mycursor.execute(sql, adr)
+mydb.commit()
+
+print(mycursor.rowcount, "record(s) deleted")
