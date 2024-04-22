e8a3daf97ec48896d8d11186e1935daf233655f7
LakshithaRJ <lakshrj139@gmail.com>
2024-04-22 15:47:52 -0500

    SQL HAVING

--- test.sql
+++ test.sql
@@ -239,3 +239,26 @@ ORDER BY COUNT(CustomerID) DESC;
 SELECT Shippers.ShipperName, COUNT(Orders.OrderID) AS NumberOfOrders FROM Orders
 LEFT JOIN Shippers ON Orders.ShipperID = Shippers.ShipperID
 GROUP BY ShipperName;
+
+-- The SQL HAVING Clause
+-- The HAVING clause was added to SQL because the WHERE keyword cannot be used with aggregate functions.
+
+-- SELECT column_name(s)
+-- FROM table_name
+-- WHERE condition
+-- GROUP BY column_name(s)
+-- HAVING condition
+-- ORDER BY column_name(s);
+
+-- The following SQL statement lists the number of customers in each country. Only include countries with more than 5 customers:
+SELECT COUNT(CustomerID), Country
+FROM Customers
+GROUP BY Country
+HAVING COUNT(CustomerID) > 5;
+
+-- The following SQL statement lists the number of customers in each country, sorted high to low (Only include countries with more than 5 customers):
+SELECT COUNT(CustomerID), Country
+FROM Customers
+GROUP BY Country
+HAVING COUNT(CustomerID) > 5
+ORDER BY COUNT(CustomerID) DESC;
